f061573877ba286e5f48102109625c5497f61ece
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-06-30 15:24:08 +0300

    added basic models for API

--- diplom_site/backend/models.py
+++ diplom_site/backend/models.py
@@ -1,4 +1,58 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **user_flags):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **user_flags)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **user_flags):
+        user_flags.setdefault('is_staff', False)
+        user_flags.setdefault('is_superuser', False)
+        return self._create_user(email, password, **user_flags)
+
+    def create_superuser(self, email, password=None, **user_flags):
+        user_flags.setdefault('is_staff', True)
+        user_flags.setdefault('is_superuser', True)
+        if user_flags.get('is_staff') is not True:
+            raise ValueError('Superuser required to be a staff member (is_stuff=True required)')
+        if user_flags.get('is_superuser') is not True:
+            raise ValueError('Superuser required to be a superuser(is_superuser=True required)')
+
+        return self._create_user(email, password, **user_flags)
+
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ['email']
+    objects = UserManager
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(_('email address'), verbose_name='Электронная почта', unique=True)
+    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
 
 
 class Shop(models.Model):
