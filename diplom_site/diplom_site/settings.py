e0be41ae0ddd3ff6f2c96e217ec05c315b3fb292
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-08-22 13:58:04 +0300

    added throttling to project

--- diplom_site/diplom_site/settings.py
+++ diplom_site/diplom_site/settings.py
@@ -139,5 +139,13 @@ AUTH_USER_MODEL = 'backend.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '100/hour'
+    }
 }
