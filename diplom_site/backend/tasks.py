5589c664b3243b6c6d43e8ef6e16d9acb8c47267
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-08-25 15:19:42 +0300

    replaced loading yaml file with async task, extended README.md

--- diplom_site/backend/tasks.py
+++ diplom_site/backend/tasks.py
@@ -1,9 +1,19 @@
+import yaml
 from django.core.mail import send_mail
 from celery import shared_task
+from requests import get
+
+from backend.models import Shop, Category, ProductInfo, Product, Parameter, ProductParameter, User
 
 
 @shared_task()
 def send_token_email(email, token):
+    """
+    Функция асинхронной рассылки токена.
+    :param email: Адрес почты.
+    :param token: Ключ объекта Token.
+    :return:
+    """
     send_mail(
         subject="Your New Token",
         message=f"\tNew token: {token}\n\nThank you!",
@@ -12,3 +22,32 @@ def send_token_email(email, token):
         fail_silently=False,
     )
     return 'email sent'
+
+
+@shared_task()
+def load_yaml_task(url, user_id):
+    user = User.objects.get(id=user_id)
+    stream = get(url).content
+    data = yaml.full_load(stream)
+    shop, _ = Shop.objects.get_or_create(name=data['shop'], user=user)
+    for category in data['categories']:
+        category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+        category_object.shops.add(shop.id)
+        category_object.save()
+    ProductInfo.objects.filter(shop_id=shop.id).delete()
+    for item in data['goods']:
+        product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+
+        product_info = ProductInfo.objects.create(product_id=product.id,
+                                                  external_id=item['id'],
+                                                  model=item['model'],
+                                                  price=item['price'],
+                                                  price_rrc=item['price_rrc'],
+                                                  quantity=item['quantity'],
+                                                  shop_id=shop.id)
+        for name, value in item['parameters'].items():
+            parameter_object, _ = Parameter.objects.get_or_create(name=name)
+            ProductParameter.objects.create(product_info_id=product_info.id,
+                                            parameter_id=parameter_object.id,
+                                            value=value)
+    return 'yaml loaded'
--- diplom_site/backend/views.py
+++ diplom_site/backend/views.py
@@ -16,7 +16,7 @@ from rest_framework import viewsets
 from backend.serializers import UserSerializer, UserUpdateSerializer, ProductInfoSerializer, \
     ProductSerializer, ContactSerializer, OrderSerializer, OrderItemSerializer
 from backend.models import Category, ProductInfo, Product, ProductParameter, Parameter, Shop, CITIES, OrderItem, User
-from backend.tasks import send_token_email
+from backend.tasks import send_token_email, load_yaml_task
 
 
 def calculate_delivery_cost(shop_city, buyer_city):
@@ -153,6 +153,7 @@ class RefreshToken(APIView):
                 old_token = request.auth
                 Token.objects.filter(key=old_token).delete()
                 token = Token.objects.create(user=user)
+                # дублируем токен на указанную почту
                 send_token_email.delay(user.email, token.key)
                 return Response({
                     'token': token.key,
@@ -186,33 +187,8 @@ class PartnerUpdate(APIView):
             except ValidationError as e:
                 return JsonResponse({'Status': False, 'Error': str(e)})
             else:
-                stream = get(url).content
-
-                data = yaml.full_load(stream)
-
-                shop, _ = Shop.objects.get_or_create(name=data['shop'], user=request.user)
-                for category in data['categories']:
-                    category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
-                    category_object.shops.add(shop.id)
-                    category_object.save()
-                ProductInfo.objects.filter(shop_id=shop.id).delete()
-                for item in data['goods']:
-                    product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
-
-                    product_info = ProductInfo.objects.create(product_id=product.id,
-                                                              external_id=item['id'],
-                                                              model=item['model'],
-                                                              price=item['price'],
-                                                              price_rrc=item['price_rrc'],
-                                                              quantity=item['quantity'],
-                                                              shop_id=shop.id)
-                    for name, value in item['parameters'].items():
-                        parameter_object, _ = Parameter.objects.get_or_create(name=name)
-                        ProductParameter.objects.create(product_info_id=product_info.id,
-                                                        parameter_id=parameter_object.id,
-                                                        value=value)
-
-                return JsonResponse({'Status': True})
+                load_yaml_task.delay(url, request.user.id)
+                return JsonResponse({'Status': 'Files are being loaded'})
 
         return JsonResponse({'Status': False, 'Errors': 'All required arguments were not provided'})
 
